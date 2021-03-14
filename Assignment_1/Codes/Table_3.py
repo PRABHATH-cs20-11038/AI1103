f3a089ddb580949296c70a23900d717e00733885
PRABHATH-cs20-11038 <80516320+PRABHATH-cs20-11038@users.noreply.github.com>
2021-03-14 14:57:24 +0530

    Add files via upload

---
+++ Assignment_1/Codes/Table_3.py
@@ -0,0 +1,15 @@
+import plotly.graph_objects as go
+
+fig = go.Figure(data=[go.Table(
+    header=dict(values=['k', '0', '1', '2', '3', '4'],
+                line_color='darkslategray',
+                fill_color='lightskyblue',
+                align='left'),
+    cells=dict(values=[['Pr(k)'],['1/16'],['1/4'],['3/8'],['1/4'],['1/16']],
+               line_color='darkslategray',
+               fill_color='lightcyan',
+               align='left'))
+])
+
+fig.update_layout(width=500, height=300)
+fig.show()
