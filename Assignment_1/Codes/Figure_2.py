69b48c4b66e819c92c6c508d6b688b7300eb668e
PRABHATH-cs20-11038 <80516320+PRABHATH-cs20-11038@users.noreply.github.com>
2021-03-16 18:08:00 +0530

    Update Figure_2.py

--- Assignment_1/Codes/Figure_2.py
+++ Assignment_1/Codes/Figure_2.py
@@ -20,7 +20,32 @@ probabilityhead_3=special.binom(n, 3)*(q)**n
 x1 = [0, 1, 2,3]
 y1 = [probabilityhead_0, probabilityhead_1, probabilityhead_2, probabilityhead_3]
 
-plt.bar(x1, y1, label="probability of no. of tails", color='g')
+#simulation practically
+z=0
+tails0=0
+tails1=0
+tails2=0
+tails3=0
+ 
+while z<100000: #simlen
+     num=[]
+     for i in range(0,3):
+           num.append(randint(0,1))
+     value=num.count(1)
+     if value==0:
+          tails0+=1
+     elif value==1:
+          tails1+=1
+     elif value==2:
+          tails2+=1
+     elif value==3:
+          tails3+=1
+     z=z+1
+
+y2 = [tails0/z,tails1/z,tails2/z,tails3/z]
+#plotting of graph
+plt.bar(x1, y1, label="Theoretical solution", color='g')
+plt.plot(x1, y2, label="Practical soltion")
 plt.plot()
 
 plt.xlabel("number of tails")
