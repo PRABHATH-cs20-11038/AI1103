c42e7ee2c4e69aa3dfd8fe1c5f2eb274881f3dd6
PRABHATH-cs20-11038 <80516320+PRABHATH-cs20-11038@users.noreply.github.com>
2021-03-18 15:19:06 +0530

    Update Figure_1.py

--- Assignment_1/Codes/Figure_1.py
+++ Assignment_1/Codes/Figure_1.py
@@ -6,28 +6,19 @@ from scipy.stats import binom
 from scipy import special
 from random import randint
 
-#parameter
-p=1/2
+#sample size
+simlen=10000
 
-#no of tossed coins
-n=2
+#Posible outcomes
+n=(0,3)
 
-#probabilty
-#theoretically 
-probabilityhead_0=special.binom(n, 0)*(p)**n
-probabilityhead_1=special.binom(n, 1)*(p)**n
-probabilityhead_2=special.binom(n, 2)*(p)**n
-
-x1 = [0, 1, 2]
-y1 = [probabilityhead_0, probabilityhead_1, probabilityhead_2]
-
-#simulation practically
+#simulation
 z=0
 heads0=0
 heads1=0
 heads2=0
  
-while z<100000: #simlen
+while z<simlen:
      num=[]
      for i in range(0,2):
            num.append(randint(0,1))
@@ -40,16 +31,21 @@ while z<100000: #simlen
           heads2+=1
      z=z+1
 
+#Simulated probability
+psim=[heads0/simlen,heads1/simlen,heads2/simlen]
 
+#Theoretical probability
+n = range(0,3)
+probabilityhead_0=special.binom(2, 0)*(1/2)**2
+probabilityhead_1=special.binom(2, 1)*(1/2)**2
+probabilityhead_2=special.binom(2, 2)*(1/2)**2
+panal = np.concatenate((probabilityhead_0,probabilityhead_1,probabilityhead_2),axis=None)
 
-y2 = [heads0/z,heads1/z,heads2/z]
-#plotting graph
-plt.bar(x1, y1, label="Theoritical solution", color='y')
-plt.plot(x1, y2, label="Practical soltion")
-plt.plot()
-
-plt.xlabel("number of heads")
-plt.ylabel("Probality")
+#Plotting
+plt.stem(n,psim, markerfmt='o', use_line_collection=True, label='Simulation')
+plt.stem(n,panal, markerfmt='o',use_line_collection=True, label='Analysis')
+plt.xlabel('number of heads')
+plt.ylabel('Probabilty')
 plt.title("Probability distribution of number of heads with two tosses of a coin")
 plt.legend()
-plt.show()
+plt.grid()
