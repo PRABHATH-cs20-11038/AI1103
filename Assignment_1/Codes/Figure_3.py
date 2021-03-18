fbbd6bb033fdf4a67277731a16ee04151a856821
PRABHATH-cs20-11038 <80516320+PRABHATH-cs20-11038@users.noreply.github.com>
2021-03-18 15:20:12 +0530

    Update Figure_3.py

--- Assignment_1/Codes/Figure_3.py
+++ Assignment_1/Codes/Figure_3.py
@@ -4,26 +4,15 @@ from scipy.stats import bernoulli
 from scipy.stats import norm
 from scipy.stats import binom
 from scipy import special
+from random import randint
 
-#parameter
-p=1/2
+#sample size
+simlen=10000
 
-#no of tossed coins
-n=4
+#Posible outcomes
+n=(0,5)
 
-#probabilty
-probabilityhead_0=special.binom(n, 0)*(p)**n
-probabilityhead_1=special.binom(n, 1)*(p)**n
-probabilityhead_2=special.binom(n, 2)*(p)**n
-probabilityhead_3=special.binom(n, 3)*(p)**n
-probabilityhead_4=special.binom(n, 4)*(p)**n
-
-x1 = [0, 1, 2, 3, 4]
-y1 = [probabilityhead_0, probabilityhead_1, probabilityhead_2, probabilityhead_3, probabilityhead_4]
-
-
-
-#simulation practically
+#simulation
 z=0
 heads0=0
 heads1=0
@@ -31,7 +20,7 @@ heads2=0
 heads3=0
 heads4=0
  
-while z<100000: #simlen
+while z<simlen:
      num=[]
      for i in range(0,4):
            num.append(randint(0,1))
@@ -48,14 +37,23 @@ while z<100000: #simlen
           heads4+=1
      z=z+1
 
-y2 = [heads0/z,heads1/z,heads2/z,heads3/z,heads4/z]
-#plotting of graph
-plt.bar(x1, y1, label="Theoretical solution", color='r')
-plt.plot(x1, y2, label="Practical soltion")
-plt.plot()
+#Simulated probability
+psim=[heads0/simlen,heads1/simlen,heads2/simlen,heads3/simlen,heads4/simlen]
 
-plt.xlabel("number of heads")
-plt.ylabel("Probality")
-plt.title("Probability distribution of number of heads with four tossing coins")
+#Theoretical probability
+n = range(0,5)
+probabilityhead_0=special.binom(4, 0)*(1/2)**4
+probabilityhead_1=special.binom(4, 1)*(1/2)**4
+probabilityhead_2=special.binom(4, 2)*(1/2)**4
+probabilityhead_3=special.binom(4, 3)*(1/2)**4
+probabilityhead_4=special.binom(4, 4)*(1/2)**4
+panal = np.concatenate((probabilityhead_0,probabilityhead_1,probabilityhead_2,probabilityhead_3,probabilityhead_4),axis=None)
+
+#Plotting
+plt.stem(n,psim, markerfmt='o', use_line_collection=True, label='Simulation')
+plt.stem(n,panal, markerfmt='o',use_line_collection=True, label='Analysis')
+plt.xlabel('number of heads')
+plt.ylabel('Probabilty')
+plt.title("Probability distribution of number of heads with two tosses of a coin")
 plt.legend()
-plt.show()
+plt.grid()
