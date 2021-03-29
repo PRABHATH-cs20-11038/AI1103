7268613bb22548ea78c90acb17f5bdcc28b225d0
PRABHATH-cs20-11038 <80516320+PRABHATH-cs20-11038@users.noreply.github.com>
2021-03-29 08:58:38 +0530

    Add files via upload

--- Assignment_5/Codes/simulation.py
+++ Assignment_5/Codes/simulation.py
@@ -6,22 +6,13 @@ mu=0
 sigma=np.sqrt(2)
 # Creating histogram
 samples = np.random.normal(mu, sigma, 100000)
-plt.hist(samples, bins = 50, density = True,
-         alpha = 1, color =(0.9, 0.1, 0.1))
-  
-# Add a title
-plt.title('Random Samples - Normal Distribution')
-  
-# Add X and y Label
-plt.xlabel('Z value')
-plt.ylabel('Frequency')
-  
-# Creating vectors X and Y
-x = np.linspace(-6, 6, 10000)
-y = (1/(4 * np.pi)**0.5) * (np.exp(-x**2 / 4))
-  
-# Creating plot
-plt.plot(x, y, 'b', alpha = 1)
 
-# Show plot
-plt.grid()
+#simulation
+count=0
+z=0
+while z<100000:
+  if samples[z]>=0:
+    count+=1
+  z+=1
+print("Simulated Probability:",count/100000)
+print("Actual probability:",1/2)
