28295e45063d6823a381c5729b5beec5c771d941
PRABHATH-cs20-11038 <80516320+PRABHATH-cs20-11038@users.noreply.github.com>
2021-04-01 09:40:16 +0530

    Update simulation.py

--- Assignment_5/Codes/simulation.py
+++ Assignment_5/Codes/simulation.py
@@ -2,17 +2,22 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
+# simlen
+simlen=100000
+# mean
 mu=0
+#variance
 sigma=np.sqrt(2)
-# Creating histogram
-samples = np.random.normal(mu, sigma, 100000)
+# creating sample list
+samples = np.random.normal(mu, sigma, simlen)
 
-#simulation
+# simulation
 count=0
 z=0
-while z<100000:
+while z<simlen:
   if samples[z]>=0:
     count+=1
   z+=1
-print("Simulated Probability:",count/100000)
+
+print("Simulated Probability:",count/simlen)
 print("Actual probability:",1/2)
